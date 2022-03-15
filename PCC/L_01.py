53dee20421d30ca37d3c7b57c5936d71c9bb355b
Alex <alexfranciscoreyes@gmail.com>
2022-03-14 20:04:12 -0500

    Notes added

--- PCC/L_01.py
+++ PCC/L_01.py
@@ -1,6 +1,8 @@
+# Python variables are read from top to bottom so if a global variable is changed at the end of a script then its final value is the most recent one
+
 message = 'Hello World!'
 print(message)
 message = 'Hellow Python Crash Course World!'
 print(message)
 
-#Peepee Poopoo
+
