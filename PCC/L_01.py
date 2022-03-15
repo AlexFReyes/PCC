dd73de4dc0293b1cee1138267aac995aa6e31800
Alex <alexfranciscoreyes@gmail.com>
2022-03-15 18:55:58 -0500

    Varibale Comments

--- PCC/L_01.py
+++ PCC/L_01.py
@@ -1,5 +1,8 @@
 # Python variables are read from top to bottom so if a global variable is changed at the end of a script then its final value is the most recent one
-
+# Variable names can only contain letters, numbers, and underscores. 
+# Variables can't start with numbers
+# Spaces can't be used in variable names so _ are often used in their place
+# Upper case letters in variable names have special meanings.
 message = 'Hello World!'
 print(message)
 message = 'Hellow Python Crash Course World!'
