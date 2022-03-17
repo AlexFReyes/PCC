c5eca2550a813e8070d231c66fb60f6b36e89f0f
Alex <alexfranciscoreyes@gmail.com>
2022-03-17 01:53:10 -0500

    done with try it yourself list edit

--- PCC/Chapter 2/Editing_Lists - Try it Yourself.py
+++ PCC/Chapter 2/Editing_Lists - Try it Yourself.py
@@ -6,8 +6,8 @@ for x in invites:
 
 # 3-5
 print('\n\n3-5\n'+invites[1])
-flakers = invites.pop(1)
-invites.insert(1,'Miyozaki')
+invites.remove('hitler')
+invites.insert(1,'miyozaki')
 for x in invites:
     print(f"Hello Mr. {x.title()} \n\tYou are cordially invited to dinner at the Reyes Estate.")
 
@@ -24,15 +24,11 @@ for x in invites:
 # 3-7
 print('\n\n3-7')
 print('Apologies to all! We will only be able to host two guests this evening.')
-for i in range(len(invites)-1):
+for i in range(len(invites)-2):
     cut_invitee = invites.pop()
     print(f'\tDear Mr. {cut_invitee.title()},\n\t\tI regret to inform you that we will not have space for you to dine with us at the Reyes Estate dinner tonight.\n\t\tSincerest regrets,\n\t\tSir Goldwell')
 for x in invites:
-    print(f'\tDear Mr. {x.title()},\n\tI am pleased  to inform you that you will be dining with us at the Reyes Estate dinner tonight if you so choose.\n\tGraciously,\n\tSir Goldwell')
+    print(f'\tDear Mr. {x.title()},\n\tI am pleased to inform you that you will be dining with us at the Reyes Estate dinner tonight if you so choose.\n\tGraciously,\n\tSir Goldwell')
+del invites[0]
 del invites[0]
 print(invites)
-
-
-
-
-
