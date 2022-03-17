55d2ff9f3c3c112888471094512c2d4dc7c53704
Alex <alexfranciscoreyes@gmail.com>
2022-03-16 20:58:43 -0500

    Began chapter 2 Lists

---
+++ PCC/Chapter 2/Lists.py
@@ -0,0 +1,5 @@
+# List - a collection of items in a particular order
+bicycles = ['trek','cannondale','redline','specialized']
+print(bicycles)
+# Lists start at 0, selecting an item from a list add [x] to the end of a var
+print(bicycles[0])
