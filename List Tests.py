837d48dfb808128219762173ee4ef318fbb6b640
Alex <alexfranciscoreyes@gmail.com>
2022-03-24 19:47:33 -0500

    Cool project

---
+++ List Tests.py
@@ -0,0 +1,105 @@
+nasdtec_list_raw = '''
+Alabama 26. Minnesota
+2. Alaska 27. Mississippi
+3. Arizona 28. Missouri
+4. Arkansas 29. Montana
+5. California 30. Nebraska
+6. Colorado 31. Nevada
+7. Connecticut 32. New Hampshire
+8. Delaware 33. New Jersey
+9. District of Columbia 34. New York
+10. DODEA 35. North Carolina
+11. Florida 36. North Dakota
+12. Georgia 37. Ohio
+13. Guam 38. Oklahoma
+14. Hawaii 39. Oregon
+15. Idaho 40. Pennsylvania
+16. Illinois 41. Rhode Island
+17. Indiana 42. South Carolina
+18. Iowa 43. Tennessee
+19. Kansas 44. Texas
+20. Kentucky 45. Utah
+21. Louisiana 46. Vermont
+22. Maine 47. Virginia
+23. Maryland 48. Washington
+24. Massachusetts 49. West Virginia
+25. Michigan 50. Wisconsin
+51. Wyoming'''.lower()
+
+state_list = '''
+Alabama
+.Alaska
+.Arizona
+.Arkansas
+.California
+.Colorado
+.Connecticut
+.Delaware
+.Florida
+.Georgia
+.Hawaii
+.Idaho
+.Illinois
+.Indiana
+.Iowa
+.Kansas
+.Kentucky
+.Louisiana
+.Maine
+.Maryland
+.Massachusetts
+.Michigan
+.Minnesota
+.Mississippi
+.Missouri
+.Montana
+.Nebraska
+.Nevada
+.New Hampshire
+.New Jersey
+.New Mexico
+.New York
+.North Carolina
+.North Dakota
+.Ohio
+.Oklahoma
+.Oregon
+.Pennsylvania
+.Rhode Island
+.South Carolina
+.South Dakota
+.Tennessee
+.Texas
+.Utah
+.Vermont
+.Virginia
+.Washington
+.West Virginia
+.Wisconsin
+.Wyoming
+'''.lower().split('.')
+
+nasdtec_list_clean = ''.join([i for i in nasdtec_list_raw if not i.isdigit()])
+n_list = nasdtec_list_clean.split('.')
+
+gn_list = []
+for i in n_list:
+    ap = i.strip()
+    gn_list.append(ap)
+
+s_list = []
+for i in state_list:
+    sa = i.strip()
+    s_list.append(sa)
+
+for i in s_list:
+    if i not in gn_list:
+        print(f'{i} is not in nasdtec')
+
+
+
+
+
+
+
+
